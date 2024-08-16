7d607bd33463aadb9d690f0cdddf382965290a66
AlexAdleyba <light_lex@mail.ru>
2024-08-16 18:14:48 +0300

    first commit

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -5,22 +5,14 @@ import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
-/*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
-* */
 
-// нужно создать правильный тип вместо any
 export type MessageType = {
     id: number
-    user: UserAvatarNameType
+    user: AvatarNameType
     message: TextTimeType
 }
 
-type UserAvatarNameType = {
+type AvatarNameType = {
     avatar: string
     name: string
 }
@@ -30,27 +22,27 @@ type TextTimeType = {
     time: string
 }
 
-// структуру объекта не менять
+
 export const message0: MessageType = {
     id: 0,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        avatar: avatar,
+        name: 'Some Name',
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
+        text: 'some textsome textsome textsome textsome textsome textsome text',
+        time: '22:00',
     },
 }
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        avatar: avatar,
+        name: 'Friend Name',
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: 'зеркальное сообщение для тренировки css',
+        time: '22:00',
     },
 }
 
@@ -59,13 +51,13 @@ const HW1 = () => {
         <div id={'hw1'}>
             <div className={s2.hwTitle}>Homework #1</div>
             <div className={s2.hw}>
-                {/*проверка отображения (не менять)*/}
+
                 <div>
                     <Message message={message0} />
                     <FriendMessage message={friendMessage0} />
                 </div>
 
-                {/*для автоматической проверки дз (не менять)*/}
+
                 <MessageSender M={Message} />
             </div>
         </div>
